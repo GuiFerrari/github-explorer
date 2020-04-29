361d8977ee2beb7120442c04085faa57d78de4cf
GuiFerrari <guiferrari10@hotmail.com>
2020-04-29 17:32:32 -0300

    Created navigation and storage repositories

--- src/pages/Repositories/index.tsx
+++ src/pages/Repositories/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+import { Header } from './style';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repositories: React.FC = () => {
-  return <h1>Repositories</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+  return <Header />;
 };
 
 export default Repositories;
